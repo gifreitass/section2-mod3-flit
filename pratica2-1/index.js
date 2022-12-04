8586a42cb470ded30fa42f617d2da5f0b097848a
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2022-12-04 15:33:55 -0300

    adicionando a terceira função

--- pratica2-1/index.js
+++ pratica2-1/index.js
@@ -39,3 +39,40 @@ function paradasViagem(passageiros, tempoHoras){
 }
 
 paradasViagem(['adulto', 'adulto', 'criança', 'adulto', 'criança', 'criança'], 5)
+
+
+
+function valorRefeicoesEmParadas(numeroParadas, passageiros){
+    // Assumindo que a primeira parada é sempre COMPLETA
+    // diminui o numero de paradas em 1
+    let numeroParadasSubEntendido = numeroParadas - 1;
+    let paradasRefCompleta = 1
+    // aumenta o numero de refCompleta em 1
+
+    const passageirosAdultos = passageiros.filter(passageiro => passageiro == 'adulto')
+    const passageirosCrianças = passageiros.filter(passageiro => passageiro == 'criança')
+
+    const refLeveAdulto = 30
+    const refCompletaAdulto = 50
+    const refLeveCrianca = 20
+    const refCompletaCrianca = 40
+
+    paradasRefCompleta += Math.floor(numeroParadasSubEntendido / 3)
+    const paradasRefLeve = numeroParadas - paradasRefCompleta
+
+    // calc ref leve adulto = qtdAdulto * valor
+    const valorRefLeveAdulto = passageirosAdultos.length * refLeveAdulto
+    const valorRefCompletaAdulto = passageirosAdultos.length * refCompletaAdulto
+    const valorRefLeveCrianca = passageirosCrianças.length * refLeveCrianca
+    const valorRefCompletaCrianca = passageirosCrianças.length * refCompletaCrianca
+
+    // calculo do valor das refs dos adultos e crianças
+    const totalAdultos = valorRefLeveAdulto * paradasRefLeve + valorRefCompletaAdulto * paradasRefCompleta
+    const totalCriancas = valorRefLeveCrianca * paradasRefLeve + valorRefCompletaCrianca * paradasRefCompleta
+
+    const valorTotalRefeicoes = totalAdultos + totalCriancas
+
+    return valorTotalRefeicoes
+}
+
+const despesasRefeicoes = valorRefeicoesEmParadas(5, ['adulto', 'criança', 'criança', 'adulto', 'adulto', 'adulto', 'criança'])
